d8bac3189ec0d5cb226a25437b8e2975d90464a4
sumitaka-sakuma <tksn.works@gmail.com>
2021-04-20 14:09:09 +0900

    message English

--- SpringStudy/src/main/java/com/example/controller/SignupController.java
+++ SpringStudy/src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +21,10 @@ public class SignupController {
 	
 	// ユーザー登録画面を表示
 	@GetMapping("/signup")
-	public String getSignup(Model model) {
+	public String getSignup(Model model, Locale locale) {
 		
 		// 性別を取得
-		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 		
 		// ユーザー登録画面に遷移
