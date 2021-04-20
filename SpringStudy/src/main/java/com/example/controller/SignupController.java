80ab02e28524987aab2474088b1422b1c04f2470
sumitaka-sakuma <tksn.works@gmail.com>
2021-04-20 15:23:20 +0900

    validation

--- SpringStudy/src/main/java/com/example/controller/SignupController.java
+++ SpringStudy/src/main/java/com/example/controller/SignupController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +28,8 @@ public class SignupController {
 	
 	// ユーザー登録画面を表示
 	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
+	public String getSignup(Model model, Locale locale, 
+			@ModelAttribute SignupForm form) {
 		
 		// 性別を取得
 		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
@@ -38,9 +41,17 @@ public class SignupController {
 	
 	// ユーザー登録処理
 	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm form) {
+	public String postSignup(Model model, Locale locale,
+			@ModelAttribute @Validated SignupForm form,
+			BindingResult bindingResult) {
 		
 		log.info(form.toString());
+		
+		// 入力チェック結果
+		if(bindingResult.hasErrors()) {
+			// NG:ユーザー登録画面に戻る
+			return getSignup(model, locale, form);
+		}
 		// ログイン画面にリダイレクト
 		return "redirect:/login";
 	}
